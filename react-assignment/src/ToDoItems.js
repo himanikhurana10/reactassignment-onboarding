f4566496f14d750b25a9b2dd48e064a80ed9e259
himansi <himani.khurana@hashedin.com>
2019-04-08 19:09:44 +0530

    checkboxes added, tags added(partially)

--- react-assignment/src/ToDoItems.js
+++ react-assignment/src/ToDoItems.js
@@ -1,38 +1,138 @@
 import React, { Component } from "react";
- 
+const ENTER_KEY = 13;
+const COMMA_KEY = 188; 
+var listItems
+
 class TodoItems extends Component {
   constructor(props) {
         super(props);
-     
+        this.state = { tags: ["work", "personal", "family"], value: "" };
+
         this.createTasks = this.createTasks.bind(this);
   }
 
-  createTasks(item) {
+  strike(strike, data) {
+      if (strike) {
+          return (
+              <p>
+                  <strike>{data}</strike>
+              </p>
+          );
+      } else {
+          return (
+              <p>{data}</p>
+          )
+      }
+  }
+ 
+
+  strikeItem(key) {
+    console.log("CLICK IN CHILD", key)
+      this.props.onItemCompleted(key);
+  } 
+//   addworktag(id){
+
+//        listItems.find((item) => {
+//         return item.key === id;}).tag.Work= true
+// }
+// addpersonaltag(id){
+
+//     listItems.find((item) => {
+//      return item.key === id;}).tag.Personal= true
+// }
+// addfamilytag(key){
+
+//     // listItems.find((item) => {
+//     //  return item.key === key;}).tag.Family= true
+//     console.log(listItems.find((item) => {
+//         return item.key === key;}).tag.Family= true)
+// }
+
+
+
+addTag() {
+    const { tags, value } = this.state;
+    let tag = value.trim();
+
+    tag = tag.replace(/,/g, "");
+
+    if (!tag) {
+      return;
+    }
+
+    this.setState({
+      tags: [...tags, tag],
+      value: ""
+    });
+  } 
+  handleKeyUp(e) {
+    const key = e.keyCode;
+
+    if (key === ENTER_KEY || key === COMMA_KEY) {
+      this.addTag();
+    }
+  }
+ 
+createTasks(item) {
+        const { tags, value } = this.state;
         return  (
         
         <div>
-        <input type ="checkbox" />
-        <p onClick={() => this.delete(item.key)} 
-                    key={item.key}>{item.text}</p>
-                    </div>
-        // <li onClick={() => this.delete(item.key)} 
-        //             key={item.key}>{item.text}</li>            
+        <input type ="checkbox"  onClick={() =>
+        //  setTimeout((listItem) => {
+        //     this.delete(item.key)  
+        //   }, 800, this._listItem)
+        //  this.strikes()
+            this.strikeItem(item.key)
+         }    
+                 key={item.key}/>
+                    {this.strike(item.done, item.text)}
+                    <ul>
+            {tags.map((tag, i) => (
+              <li key={tag + i} className="tag">
+                {tag}
+              </li>
+            ))}
+          </ul>
+          <input
+            type="text"
+            placeholder="
+                         
+                         tag..."
+            value={value}
+            ref="tag"
+            onKeyUp={this.handleKeyUp}
+          />
+                    {/* <ul>
+                        <li type="none"/>
+                          <input type="text" placeholder="add tag" value ={}/>
+                        {/* <input type="text" onClick={() => 
+                            this.addworktag(item.key)} />
+                            Work
+                        <input type="checkbox" onClick={() => 
+                            this.addpersonaltag(item.key)} />
+                            Personal
+                        <input type="checkbox" onClick={() => 
+                            this.addfamilytag(item.key)} />
+                            Family   */}
+                 
+             
+                    </div>           
         )
       }
     
   delete(key) {
-    //this.props.listItems[key]= this.props.<strike></strike    
     this.props.delete(key);
-      }  
+  }
  
   render() {
+    console.log("props : - ", this.props)
     var todoEntries = this.props.entries;
-    var listItems = todoEntries.map(this.createTasks);
- 
+    listItems = todoEntries.map(this.createTasks);
+      
     return (
-      // for loop, include checkboxes
       <ul className="theList">
-          {listItems}
+          {listItems.sort((a, b) => a.key - b.key)}
       </ul>
     );
   }
--- react-assignment/src/ToDoList.js
+++ react-assignment/src/ToDoList.js
@@ -11,14 +11,19 @@ class TodoList extends Component {
        
         this.addItem = this.addItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
+        this.markItemCompleted = this.markItemCompleted.bind(this);
+
       }
-      addItem(e) {
+    addItem(e) {
         if (this._inputElement.value !== "") {
           var newItem = {
             text: this._inputElement.value,
-            key: Date.now()
+            key: Date.now(),
+            tag : String,
+            done : false 
           };
-          console.log("VAUE IS", this._inputElement.value)
+    
+    //console.log("VAUE IS", this._inputElement.value)
        
           this.setState((prevState) => {
             return { 
@@ -33,6 +38,18 @@ class TodoList extends Component {
            
         e.preventDefault();
       }
+      markItemCompleted(key) {
+          console.log("CLICKED ON ", key);
+        var updatedItems = this.state.items.map(item => {
+         if (key === item.key)
+           item.done = !item.done;
+         return item;
+        });
+     
+        this.setState({
+         items: [].concat(updatedItems)
+         });
+      }
       deleteItem(key) {
         var filteredItems = this.state.items.filter(function (item) {
           return (item.key !== key);
@@ -42,6 +59,7 @@ class TodoList extends Component {
           items: filteredItems
         });
       }
+
       render() {
         return (
           <div className="todoListMain">
@@ -54,7 +72,7 @@ class TodoList extends Component {
               </form>
             </div>
             <ToDoItems entries={this.state.items}
-                      delete={this.deleteItem}/>
+                      delete={this.deleteItem}  onItemCompleted={this.markItemCompleted}/>
           </div>
         );
       }
